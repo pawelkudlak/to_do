af12503af0ae2e016e26511ea471f99d0d25deae
pawelkudlak <coodypl@gmail.com>
2022-10-04 21:41:21 +0200

    icon adding and removing

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
+let idCreator = 0;                                                  //let bo const wysypuje apke
+
 function addNewTask() {
     const taskData = document.getElementById("toDoThing").value;    //podane zadanie
-    const divNewTask = document.createElement("li");               //utworzenie diva
+    const divNewTask = document.createElement("li");                //utworzenie diva
+
+    divNewTask.setAttribute("id", idCreator);
+
     const tasksArea = document.getElementById("tasksList");         //oznaczenie miejsca taska
-    const task = document.createTextNode(taskData);                 //dodanie treści zadania 
+    const task = document.createTextNode(taskData);                 //dodanie treści zadania
+
+    const deleteButton = document.createElement("button");
+    deleteButton.setAttribute("class", "deleteBtn");
+    deleteButton.setAttribute("type", "submit");
+    deleteButton.setAttribute("onclick", `deleteTask(${idCreator})`);
 
     if (taskData === "") {                                          //walidacja czy zostało coś wprowadzone
         alert("You don't add anything, pls try again!")
     } else {
         tasksArea.appendChild(divNewTask);
         divNewTask.appendChild(task);
+        divNewTask.appendChild(deleteButton);
     }
+
+    idCreator += 1;
+    document.getElementById("addTaskForm").reset();
 }
 
-
+function deleteTask(id) {
+    const taskToDelete = document.getElementById(id);
+    taskToDelete.parentNode.removeChild(taskToDelete);
+}
 
 // const tasks = document.getElementsByTagName("LI");
 // for (const i = 0; i < tasks.length; i++) {
